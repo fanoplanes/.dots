c49ce76f886cb6dc145750ecd6d02645df149cb4
fanoplanes <fanoplanes@tilde.team>
2023-02-22 16:52:05 +0100

    >:3

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/bin/env bash
 
-mkdir -p ~/.config/bspwm
-ln -s bspwmrc ~/.config/bspwm/
-ln -s external-rules.sh ~/.config/bspwm/
+mkdir -p /home/fanoplanes/.config/bspwm
+ln -rs bspwmrc /home/fanoplanes/.config/bspwm/
+ln -rs external-rules.sh /home/fanoplanes/.config/bspwm/
 
-mkdir -p ~/.config/alacritty
-ln -s alacritty.yml ~/.config/alacritty/
+mkdir -p /home/fanoplanes/.config/alacritty
+ln -rs alacritty.yml /home/fanoplanes/.config/alacritty/
 
-mkdir -p ~/.config/nvim
-ln -s init.vim ~/.config/nvim/
+mkdir -p /home/fanoplanes/.config/nvim
+ln -rs init.vim /home/fanoplanes/.config/nvim/
 
-mkdir -p ~/.config/picom
-ln -s picom.conf ~/.config/picom/
+mkdir -p /home/fanoplanes/.config/picom
+ln -rs picom.conf /home/fanoplanes/.config/picom/
 
-mkdir -p ~/.config/polybar
-ln -s config.ini ~/.config/polybar/
+mkdir -p /home/fanoplanes/.config/polybar
+ln -rs config.ini /home/fanoplanes/.config/polybar/
 
-mkdir -p ~/.config/sxhkd
-ln -s sxhkdrc ~/.config/sxhkd/
+mkdir -p /home/fanoplanes/.config/sxhkd
+ln -rs sxhkdrc /home/fanoplanes/.config/sxhkd/
 
-mkdir -p ~/.config/rofi
-ln -s nord.rasi ~/.config/rofi/
-ln -s config.rasi ~/.config/rofi/
+mkdir -p /home/fanoplanes/.config/rofi
+ln -rs nord.rasi /home/fanoplanes/.config/rofi/
+ln -rs config.rasi /home/fanoplanes/.config/rofi/
 
-ln -s .bashrc ~/
-ln -s .vimrc ~/
-ln -s .Xresources ~/
-ln -s .xinitrc ~/
+ln -rs .bashrc /home/fanoplanes/
+ln -rs .vimrc /home/fanoplanes/
+ln -rs .Xresources /home/fanoplanes/
+ln -rs .xinitrc /home/fanoplanes/
 
