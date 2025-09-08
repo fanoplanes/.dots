f507d7679fcff8637976fcb6ba1448eff4b4b89c
fanoplanes <pontryagin@protonmail.com>
2025-09-08 19:08:11 +0200

    Added default dirvish layout, added emms to doom,

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -59,6 +59,8 @@
        ;; Other attributes are displayed in the order they appear in this list.
        '(file-size))))
 
+(after! dirvish
+  (setq dirvish-default-layout '(0 0 0.55)))
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
